018e1690bf2041826837870d5a17e051deeb0476
Arrron <basedwon@protonmail.com>
2022-04-27 21:43:41 -0700

    added blank test and tweaked m'code

--- servn.js
+++ servn.js
@@ -1,66 +1,59 @@
 #!/usr/bin/env node
 const log = console.log.bind(console)
+const exists = obj => obj !== 'undefined'
 
 const fs = require('fs'),
       url = require('url'),
       path = require('path'),
-      arrrg = require('arrrg'),
+      Arrrg = require('arrrg'),
       watchify = require('watchify'),
       Transform = require('stream').Transform,
       Server = require('socket.io').Server,
       browserify = require('browserify')
 
-const defs = {
+const opts = Arrrg([
+  { name: 'servn', type: String, command: true, swap: 'docroot', anon: false },
+  { name: 'host', type: String, help: 'define the host' },
+  { name: 'file', type: String, aliases: ['f'], help: 'define the entry file' },
+  { name: 'entry', type: String, aliases: ['e'], help: 'define the entry path' },
+  { name: 'port', type: Number, aliases: ['p'], help: 'define the port' },
+  { name: 'index', type: String, aliases: ['i'], help: 'html file (index.html)' },
+  { name: 'dir', type: String, aliases: ['d'], help: 'define the TLS cert directory' },
+  { name: 'tls', type: b => b ? 'https' : 'http', aliases: ['s'], help: 'use tls', swap: 'proto' },
+  { name: 'cert', type: String, aliases: ['c'], help: 'define the TLS cert' },
+  { name: 'key', type: String, aliases: ['k'], help: 'define the TLS key' },
+  { name: 'watch', type: String, aliases: ['w'], help: 'paths to watch', array: true },
+  { name: 'help', type: Boolean, aliases: ['h', 'help'], help: 'show this dialog' },
+], {
   docroot: process.env.DOCROOT || '.',
   host: process.env.HOST || 'localhost',
   port: process.env.PORT || 8080,
-  tls: typeof process.env.TLS !== 'undefined' ? process.env.TLS : true,
+  tls: exists(process.env.TLS) ? process.env.TLS : false,
   dir: process.env.DIR || path.resolve(__dirname, '../../conf/certs'),
+  file: process.env.FILE || 'main.js',
+  entry: process.env.ENTRY || null,
+  index: process.env.INDEX || 'index.html',
   cert: process.env.CERT || 'localhost.pem',
   key: process.env.KEY || 'localhost-key.pem',
-  file: process.env.FILE || 'main.js',
-  index: process.env.INDEX || 'index.html',
   watch: process.env.WATCHERS || [],
-}
-defs.entry = process.env.ENTRY || path.resolve(defs.docroot, defs.file)
-const definition = [
-  { name: 'servn', type: String, command: true, default: defs.docroot },
-  { name: 'host', type: String, help: 'define the host', default: defs.host },
-  { name: 'file', type: String, aliases: ['f'], help: 'define the entry file', default: defs.file },
-  { name: 'entry', type: String, aliases: ['e'], help: 'define the entry path', default: defs.entry },
-  { name: 'port', type: Number, aliases: ['p'], help: 'define the port', default: defs.port },
-  { name: 'tls', type: Boolean, aliases: ['s'], help: 'whether to use https or http', default: defs.tls },
-  { name: 'dir', type: String, aliases: ['d'], help: 'define the TLS cert directory', default: defs.dir },
-  { name: 'cert', type: String, aliases: ['c'], help: 'define the TLS cert', default: defs.cert },
-  { name: 'key', type: String, aliases: ['k'], help: 'define the TLS key', default: defs.key },
-  { name: 'index', type: String, aliases: ['i'], help: 'html file (index.html)', default: defs.index },
-  { name: 'watchers', type: str => str.split(' '), aliases: ['w'], help: 'paths to watch', default: defs.watch, spaces: true },
-  { name: 'help', type: Boolean, aliases: ['h', 'help'], help: 'show this dialog' },
-]
-const examples = [
+}, [
   `servn`,
   `servn . -p 3000`,
   `servn ~/project --host example.com --file index.js`,
-]
+])
+// log(opts)
+if (opts.help) return opts.showHelp()
 
-const opts = arrrg(definition, examples)
-if (opts.help)
-  return opts.showHelp()
-
-opts.protocol = opts.tls ? 'https' : 'http'
-delete opts.tls
-delete opts._
-opts.docroot = opts.servn
-delete opts.servn
+opts.entry = opts.entry || path.resolve(opts.docroot, opts.file)
 opts.key = path.resolve(opts.dir, opts.key)
 opts.cert = path.resolve(opts.dir, opts.cert)
 opts.index = path.resolve(opts.docroot, opts.index)
-opts.watchers = opts.watchers.map(w => path.resolve(w))
-const watch = fs.readdirSync(opts.docroot)
+
+opts.watch = fs.readdirSync(path.resolve(opts.docroot))
   .filter(f => ['js', 'html', 'css'].includes(f.split('.').pop()))
   .map(f => path.resolve(opts.docroot, f))
   .filter(f => f !== opts.entry)
-  .concat(opts.watchers)
+  .concat(opts.watch.map(w => path.resolve(w)))
   
 const mimeTypes = {
   '.css' : 'text/css',
@@ -77,10 +70,10 @@ const mimeTypes = {
   '.doc' : 'application/msword'
 }
 
-const { docroot, host, port, file: entryFile, entry, protocol, watchers, index: indexFile } = opts
-const http = protocol === 'http' ? require('http') : require('https')
+const { docroot, host, port, file: entryFile, entry, proto, watch, index: indexFile } = opts
+const http = proto === 'http' ? require('http') : require('https')
 const srvOpts = {}
-if (protocol === 'https') {
+if (proto === 'https') {
   srvOpts.cert = fs.readFileSync(opts.cert)
   srvOpts.key = fs.readFileSync(opts.key)
 }
@@ -106,7 +99,7 @@ b.transform((file, opts) => {
       this.emit('file', w)
     const data = `\n\n(function () {
       const { io } = require('socket.io-client')
-      const socket = io("${protocol}://${host}:${port}", { transports: ['websocket'] })
+      const socket = io("${proto}://${host}:${port}", { transports: ['websocket'] })
       // socket.on('connect', () => console.log('connected'))
       socket.on('bundle', () => window.location.reload())
     })()\n\n`
@@ -124,7 +117,7 @@ b.bundle().on('data', data => {}).on('end', () => {
 })
 
 server.on('request', (req, res) => {
-  console.log(`${ req.method } ${ req.url }`)
+  log(`${ req.method } ${ req.url }`)
   const parsedUrl = url.parse(req.url)
   let pathname = path.resolve(docroot, (`.${ parsedUrl.pathname }`)
     .replace(/\%2520/g, '%20')
@@ -140,8 +133,17 @@ server.on('request', (req, res) => {
     return res.end(`<!doctype html><html lang="en"><head><meta charset="utf-8"><title>Page Not Found</title> <meta name="viewport" content="width=device-width, initial-scale=1"><style>*{line-height: 1.2; margin: 0;}html{color: #888; display: table; font-family: sans-serif; height: 100%; text-align: center; width: 100%;}body{display: table-cell; vertical-align: middle; margin: 2em auto;}h1{color: #555; font-size: 2em; font-weight: 400;}p{margin: 0 auto; width: 280px;}@media only screen and (max-width: 280px){body, p{width: 95%;}h1{font-size: 1.5em; margin: 0 0 0.3em;}}</style></head><body><h1>Page Not Found</h1><p>Sorry, but the page you were trying to view does not exist.</p></body></html>`)
   }
   if (fs.statSync(pathname).isDirectory()) pathname += '/index' + ext
+  // log('let us create', { pathname })
   fs.readFile(pathname, (err, data) => {
     if (err) {
+      if (parsedUrl.pathname === '/') {
+        // there is no default index.html, let's create one..
+        res.setHeader('Content-type', 'text/html')
+        return res.end(`<!doctype html><html><head><meta charset="utf-8"><title>Servn</title></head>
+          <body style="background:#222;color:white;"><script src="bundle.js"></script>
+            <h1>Servn</h1>
+          </body></html>`)
+      }
       res.statusCode = 500
       res.end(`Error getting the file: ${err}.`)
     } else {
@@ -151,5 +153,4 @@ server.on('request', (req, res) => {
   })
 })
 
-server.listen(parseInt(port), host, () => 
-  console.log(`\n<<< Server listening on ${protocol}://${host}:${port} >>>\n`))
+server.listen(parseInt(port), host, () => log(`\n<<< Server listening on ${proto}://${host}:${port} >>>\n`))
---
+++ servn.test.js
@@ -0,0 +1,3 @@
+test('Add two numbers', () => {
+  expect(2 + 3).toBe(5)
+})
